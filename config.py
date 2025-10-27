89a2a91626f6c36f833e71f7494033891588801e
andriy-metelsky <metelsky.andrey@gmail.com>
2025-10-27 21:43:22 +0100

    updated config.py

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
-# BASE_URL = "https://www.instagram.com"
-# EMAIL = "#########+test@gmail.com"  # an example, please add a real account to test with
-# PASSWORD = "##########"             # an example, please add a real account to test with
-# ULL_NAME = "Insta Test"
-# USERNAME = "insta_se_pytest"
-
 BASE_URL = "https://www.instagram.com"
-EMAIL = "metelsky.andrey+test@gmail.com"  # just an example
-PASSWORD = "A8iLifzC)A4xFZ@"
+EMAIL = "#########+test@gmail.com"  # an example, please add a real account to test with
+PASSWORD = "##########"             # an example, please add a real account to test with
 ULL_NAME = "Insta Test"
 USERNAME = "insta_se_pytest"
